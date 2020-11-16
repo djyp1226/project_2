5386f3e5048804f780a37d71822e52b27802bc2a
djyp1226 <72421988+djyp1226@users.noreply.github.com>
2020-11-16 16:38:31 +0900

    edit
    201116

--- assets/js/story.js
+++ assets/js/story.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
     console.log(document.querySelector('path').getTotalLength());
 
-    var scrollT = $(this).scrollTop();
-
+    var timer;
+    
     $(window).on('scroll', function () {
+
+        var scrollT = $(this).scrollTop();
+
         clearTimeout(timer);
         timer = setTimeout(function () {
-            scrollT = $(this).scrollTop();
-
             $('.fade').each(function () {
                 if (scrollT > $(this).offset().top - 300) $(this).addClass('on');
             });
-
-            $(window).trigger('scroll');
-
             //page2만 네비 색상 바꾸기
-            $(window).scroll(function () {
-                var scrollT = $(this).scrollTop();
-                if (scrollT > 50) {
+                if (scrollT > $(this).offset().top - 360) {
                     $('#header').addClass('bg_change');
                 } else {
                     $('#header').removeClass('bg_change');
                 }
             });
-        });
+
+        //blue line
+        if(scrollT > $('.storybox').offset().top - 350){
+           console.log('123');
+           $('.blue_line path').addClass('on');
+        }  
 
     });
-    
 });
